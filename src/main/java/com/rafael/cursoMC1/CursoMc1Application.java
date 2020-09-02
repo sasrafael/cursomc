19b13f17b8a46572cd43e16776dc558e25215faf
sasrafael <sasrafael.adamo@gmail.com>
2020-09-02 16:59:35 -0300

    Projeto criado

---
+++ src/main/java/com/rafael/cursoMC1/CursoMc1Application.java
@@ -0,0 +1,13 @@
+package com.rafael.cursoMC1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/cursoMC1/CursoMc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.cursoMC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
